91d4aaad623beecdda407dca5e2fd7d9cc6d1379
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-25 18:26:11 +0800

    JS Objects

---
+++ algorithms-and-data-structures/javascript/objects.js
@@ -0,0 +1,27 @@
+/*
+ * Objects in JavaScript
+ */
+
+// an object
+const person = {
+  name: "Mohammad Al Zouabi",
+  gender: "Male",
+  nationality: "Syrian",
+  birthPlace: "Daraa",
+};
+console.table(person);
+
+// access: O(1)
+console.table([person["name"], person.gender]);
+
+// insertion: O(1)
+person["height"] = 192;
+console.table(person);
+
+// removal: O(1)
+delete person["height"];
+console.table(person);
+
+// Keys and entries: O(n)
+console.table(Object.keys(person));
+console.table(Object.entries(person));
