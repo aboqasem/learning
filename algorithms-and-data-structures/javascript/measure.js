dbf6beb46b5f7449a50a468eaafc2d6eb0c85c9e
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-26 14:59:08 +0800

    Cleaner array arguments logging

--- algorithms-and-data-structures/javascript/measure.js
+++ algorithms-and-data-structures/javascript/measure.js
@@ -1,10 +1,20 @@
 module.exports = (fn, args) => {
+  const argsStr = args
+    .map((v) => {
+      if (Array.isArray(v)) {
+        const isLarge = v.length > 3;
+        const arr = isLarge ? v.slice(0, 3) : v;
+        return `[${arr.join(", ")}${".".repeat(isLarge * 3)}]`;
+      }
+      return v;
+    })
+    .join(", ");
   const now = Date.now();
   const output = fn(...args);
   const time = Date.now() - now;
   console.table({
     "Function name": fn.name,
-    Arguments: args.join(", "),
+    Arguments: argsStr,
     Output: output,
     "Time taken": `${time}ms`,
   });
