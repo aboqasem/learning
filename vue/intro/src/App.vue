a16451b0d17ac5c9b1d1a06a96b8c3a6f61e0374
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-25 14:46:32 +0800

    Methods

--- vue/intro/src/App.vue
+++ vue/intro/src/App.vue
@@ -2,24 +2,30 @@
   <div class="nav-bar">
     <h1 style="text-align: center; margin: 0">{{ name }}</h1>
   </div>
+  <div class="cart">Cart({{ cart.length }})</div>
   <ul>
-    <div class="product-display">
-      <div class="product-container">
-        <li v-for="{ name, description, imgs } in products" :key="name">
+    <li v-for="({ name, description, imgs, stock }, i) in products" :key="name">
+      <div class="product-display">
+        <div class="product-container">
           <ul>
             <li v-for="img in imgs" :key="img">
               <div class="product-image">
-                <img :src="img" :alt="img" />
+                <img :src="img" :alt="description" />
               </div>
             </li>
           </ul>
           <div class="product-info">
             <h2>{{ name }}</h2>
             <p>{{ description }}</p>
+            <div v-if="stock > 0">
+              In Stock: {{ stock }}
+              <p class="button" @click="addToCart(products[i])">Add to Cart</p>
+            </div>
+            <p v-else>Out of Stock</p>
           </div>
-        </li>
+        </div>
       </div>
-    </div>
+    </li>
   </ul>
 </template>
 
@@ -29,6 +35,7 @@ export default {
   data() {
     return {
       name: "Not So Shoppy",
+      cart: [],
       products: [
         {
           name: "Socks",
@@ -37,9 +44,25 @@ export default {
             "./assets/images/socks_blue.jpg",
             "./assets/images/socks_green.jpg",
           ],
+          stock: 10,
+        },
+        {
+          name: "Shoes",
+          description: "Pair of shoes to get you walky.",
+          imgs: [
+            "./assets/images/socks_blue.jpg",
+            "./assets/images/socks_green.jpg",
+          ],
+          stock: 3,
         },
       ],
     };
+  },
+  methods: {
+    addToCart(item) {
+      this.cart.push(item);
+      item.stock--;
+    },
   },
 };
 </script>
