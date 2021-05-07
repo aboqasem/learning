b46b025db86ba47b06a3416df78759c363b995a0
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-05-07 16:06:07 +0800

    Init testing with cucumber and selenium

---
+++ testing/cucumber-selenium/src/test/java/runners/TestRunner.java
@@ -0,0 +1,12 @@
+package runners;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestRunner {
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
