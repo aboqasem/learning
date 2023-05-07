73b9be1efda457df7bbaf2c84c83a42c099f58e8
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2023-05-07 15:17:10 +0800

    feat(minigrep): accept cli args

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for \"{query}\" in file \"{file_path}\"...");
 }
