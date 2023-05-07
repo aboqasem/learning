ca9d34bd7456421dcce86ad43e4b64f3d71a6c19
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2023-05-07 15:37:07 +0800

    feat(minigrep): read file content

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +7,12 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for \"{query}\" in file \"{file_path}\"...");
+
+    let text = fs::read_to_string(file_path)
+        .unwrap_or_else(|e| {
+            eprintln!("Unable to read file: {}", e);
+            process::exit(1);
+        });
+
+    println!("With text:\n{text}");
 }
